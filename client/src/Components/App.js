90621f93a669cfbf075ba7641c8c2d89c4154eb4
Thanooj <thanoojjay@gmail.com>
2021-01-24 21:50:05 -0500

    minor UI updates

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import LandingPage from './LandingPage';
 import ChartSelector from '../ChartComponents/ChartsPage';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
       <ChartSelector />
     </div>
   );
